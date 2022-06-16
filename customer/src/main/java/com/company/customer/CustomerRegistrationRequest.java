9b180a3e0eb1bbae0d13174acf86e10392761a89
TONOYAN SERGEY <A776517>
2022-06-16 21:29:55 +0400

    database layer impl

---
+++ customer/src/main/java/com/company/customer/CustomerRegistrationRequest.java
@@ -0,0 +1,4 @@
+package com.company.customer;
+
+public record CustomerRegistrationRequest(String firstName, String lastName, String email) {
+}
---
+++ customer/src/main/java/com/company/customer/CustomerService.java
@@ -0,0 +1,19 @@
+package com.company.customer;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public record CustomerService(CustomerRepository customerRepository) {
+
+    public void registerCustomer(CustomerRegistrationRequest request) {
+        Customer customer = Customer.builder()
+                .firstName(request.firstName())
+                .lastName(request.lastName())
+                .email(request.email())
+                .build();
+
+        // TODO: check if email is valid
+        // TODO: check if email is not taken
+        customerRepository.save(customer);
+    }
+}
